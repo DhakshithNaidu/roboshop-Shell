5189b0c8880b8cd1b494003ff35d04f5c68bd319
devops-76s <kasi.bommu@gmail.com>
2026-01-03 06:36:03 +0530

    installing mongodb

---
+++ mongodb.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="\tmp\$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>>LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+       echo -e "$2...$R FAILED $N"
+    else
+       echo -e "$2...$G Success $N"
+    fi      
+}
+    if [ $ID -ne 0 ] 
+    then
+       echo -e "$R ERROR :: Please run this script with root access $N"
+       exit 1 # you can give other then 0
+    else
+       echo "Your are root user"
+    fi
+
+    cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOGFILE
+
+    VALIDATE $? "Copied Mongodb repo"
+
+    dnf install mongodb-org -y &>>LOGFILE
+
+    VALIDATE $? "Installing MongoDB"
+
+    Systemctl enable mongodb
+
+    VALIDATE $? "Enabling MongoDB"
+
+    Systemctl start mongodb
+
+    VALIDATE $? "Starting MongoDB"
+
+    Sed -i "s/127.0.0.1/0.0.0.0/g" \etc\mongodb.conf &>>LOGFILE
+
+    VALIDATE $? "Remote access to mongoDB"
+
+    Systemctl restart mongod &>>LOGFILE
+
+    VALIDATE $? "Restarting MongoDB"   
+         
